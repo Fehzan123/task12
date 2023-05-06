d82a542adc3e4e14675d7d8b0cb9f2b08a747ac1
Fehzan123 <mohdfehzan1998@gmail.com>
2023-05-06 16:38:27 +0530

    thar swape

---
+++ MHINDRA TAR R PROJECT/thar.js
@@ -0,0 +1,28 @@
+var a=document.getElementById('gold');
+a.addEventListener('click',addImag);
+
+function addImag(){
+    var img=document.getElementById('car');
+    img.src='gold.jpeg';
+}
+var red=document.getElementById('red');
+red.addEventListener('click',redImag);
+
+function redImag(){
+    var img=document.getElementById('car');
+    img.src='red.jpeg';
+}
+var red=document.getElementById('black');
+black.addEventListener('click',blackImag);
+
+function blackImag(){
+    var img=document.getElementById('car');
+    img.src='black.jpeg';
+}
+var wit=document.getElementById('whit');
+wit.addEventListener('click',whitImag);
+
+function whitImag(){
+    var img=document.getElementById('car');
+    img.src='with.jpeg';
+}
